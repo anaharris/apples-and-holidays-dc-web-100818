611daf52121b4c543fe8ed090bb3ec4411aba06c
Ana Harris <vana1990@gmail.com>
2018-10-02 00:34:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   holidays = []
   supplies = []
   holiday_hash.each do |season, holiday|
-    season.to_s 
+    season_string = season.to_s 
     holiday.each do |attribute, value|
       
     end
