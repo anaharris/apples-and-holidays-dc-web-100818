e7b15a73d0521e3bd993abf56045baf23fd05869
Ana Harris <vana1990@gmail.com>
2018-10-02 00:37:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,6 @@ def all_supplies_in_holidays(holiday_hash)
     seasons << cap_season.capitalize 
       holiday.each do |attribute, value|
       cap_holiday = (holiday.to_s).split
-       
       (cap_holiday.capitalize).join(" ")
     end
   end
