2b892834ecf134e70f791d06502a1f21a8cd6bd8
Ana Harris <vana1990@gmail.com>
2018-10-02 01:36:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_list << "#{season.to_s.capitalize}: \n"
     
       holiday.each do |supply, value|
-        holiday_list << "\t#{.to_s.split("_").map(&:capitalize!).join(" ")}: #{value.join(", ")}"
+        holiday_list << "\t#{holiday.to_s.split("_").map(&:capitalize!).join(" ")}: #{value.join(", ")}"
     end
   end
    return holiday_list
