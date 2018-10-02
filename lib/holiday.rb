01e33bc4771c3cc312bcb8bb7f5af8ddcf502109
Ana Harris <vana1990@gmail.com>
2018-10-02 00:36:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
     seasons << cap_season.capitalize 
       holiday.each do |attribute, value|
       cap_holiday = holiday.to_s
+      cap_holiday
     end
   end
 end
